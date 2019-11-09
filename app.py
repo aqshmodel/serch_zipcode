d2f9b5b84ec7f0fd0d3e05d372ab4916da7c8f00
takahiro.tsukada <aqsh.reprom@gmail.com>
2019-11-09 14:38:37 +0900

    リファクタリング 不要な変数を削除

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import requests
 zipcode = '0287405'
 
 response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}')
-
 # print(response)  # ステータスコード200が返ってくる 正常値,リクエスト成功
-# print(response.text)  # 引数に対する結果が返ってくる
-data = response.json()  # 引数に対する結果が返ってくる
-results = data['results'][0]
+# print(response.text)  # jsonの結果がまるごと返ってくる
+# .json()でjsonのデータをパースする
+results = response.json()['results'][0]
 都道府県 = results["address1"]
 市区町村名 = results["address2"]
 町域名 = results["address3"]
