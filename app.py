0d11a453f88420f3c91947072fab5abdb5472354
takahiro.tsukada <aqsh.reprom@gmail.com>
2019-11-09 12:10:21 +0900

    Requestsして住所情報を表示できる機能を実装

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 import requests
 
+response = requests.get('http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287405')
+
+print(response)  # ステータスコード200が返ってくる 正常値,リクエスト成功
+print(response.text)  # 引数に対する結果が返ってくる
+
