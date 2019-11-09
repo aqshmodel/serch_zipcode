d4119a1ff807c696cee7f88d9b7ba9f17133a323
takahiro.tsukada <aqsh.reprom@gmail.com>
2019-11-09 15:09:01 +0900

    処理をmain関数に移譲

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import requests
 
-zipcode = '0287405'
 
-response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}')
-# print(response)  # ステータスコード200が返ってくる 正常値,リクエスト成功
-# print(response.text)  # jsonの結果がまるごと返ってくる
-# .json()でjsonのデータをパースする
-results = response.json()['results'][0]
-都道府県 = results["address1"]
-市区町村名 = results["address2"]
-町域名 = results["address3"]
+def main():
+    zipcode = '0287405'
+    response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}')
+    # print(response)  # ステータスコード200が返ってくる 正常値,リクエスト成功
+    # print(response.text)  # jsonの結果がまるごと返ってくる
+    # .json()でjsonのデータをパースする
+    results = response.json()['results'][0]
+    都道府県 = results["address1"]
+    市区町村名 = results["address2"]
+    町域名 = results["address3"]
+    address = f'{都道府県}{市区町村名}{町域名}'
+    print(address)
 
-address = f'{都道府県}{市区町村名}{町域名}'
 
-print(address)
+if __name__ == '__main__':
+    main()
