a766b662d39afea49db88c5b9a60ff85d95b36db
takahiro.tsukada <aqsh.reprom@gmail.com>
2019-11-09 15:16:40 +0900

    search_address関数に処理をまとめた

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 import requests
 
 
-def main():
-    zipcode = '0287405'
+def search_address(zipcode):
     response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}')
-    # print(response)  # ステータスコード200が返ってくる 正常値,リクエスト成功
-    # print(response.text)  # jsonの結果がまるごと返ってくる
-    # .json()でjsonのデータをパースする
     results = response.json()['results'][0]
     都道府県 = results["address1"]
     市区町村名 = results["address2"]
     町域名 = results["address3"]
     address = f'{都道府県}{市区町村名}{町域名}'
+    return address
+
+
+def main():
+    zipcode = '0287405'
+    address = search_address(zipcode)
+
     print(address)
 
 
