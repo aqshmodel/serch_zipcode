d462e753456ed690427bea0ee2af2c019c6629ca
takahiro.tsukada <aqsh.reprom@gmail.com>
2019-11-09 13:28:32 +0900

    ユーザーの入力した郵便番号を検索できる機能を実装

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import requests
 
-response = requests.get('http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287405')
+zipcode = input('郵便番号は？(ハイフン無し7桁)は？ > ')
+
+response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}')
 
 print(response)  # ステータスコード200が返ってくる 正常値,リクエスト成功
 print(response.text)  # 引数に対する結果が返ってくる
-
